acd0b5399a1d4c9946b33ec8e0ad2ffab5c95e51
Sameer Gautam <sg10682002@gmail.com>
2023-09-27 23:40:44 +0530

    Chat app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import { Route } from 'react-router-dom/cjs/react-router-dom.min';
 import './App.css';
+import Homepage from './Pages/Homepage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Route path="/" component = {Homepage} />
+     <Route path="/chats" />
+
     </div>
   );
 }
